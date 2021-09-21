a89701e2a7475897bd21ac946619f566beb33431
Samita Bajpai <samita.bajpai@hcl.com>
2021-09-21 10:44:53 +0530

    modified the method convert to string

--- utils/__tests__/convertStrToObj.test.js
+++ utils/__tests__/convertStrToObj.test.js
@@ -4,8 +4,18 @@ import converStrToObj from "../convertStrToObj";
 describe('to convert string to object', () => {
     
     it('price formatter will work', () => {
-        let string = 'this is string';
-        expect(typeof converStrToObj(string)).toBe(typeof {});
-    });
+        
+        let thestring = '{"Name":"samita Doe","Email":"john@doe.com","Address":"123 Doe Street"}';
+        expect(typeof converStrToObj(thestring)).toBe(typeof {});
+        });
 
+     it('price formatter will work', () => {
+        let thestring = '';
+       expect(typeof converStrToObj(thestring)).toBe(typeof {});
+     });
+
+     it('price formatter will work', () => {
+        let thestring = {};
+       expect(typeof converStrToObj(thestring)).toBe(typeof {});
+     });
 })
