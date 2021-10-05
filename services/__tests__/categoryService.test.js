8811a399e95dbc663c064b18b4513eed129245c8
Samita Bajpai <samita.bajpai@hcl.com>
2021-10-05 10:18:37 +0530

    added cart service

--- services/__tests__/categoryService.test.js
+++ services/__tests__/categoryService.test.js
@@ -1,8 +1,6 @@
 import CategoryService from "../category/categoryService";
 
 const data = {
-  limit: 10,
-  offset: 0,
   total: 377,
   results: [
     {
@@ -11,35 +9,8 @@ const data = {
       name: [Object],
       slug: [Object],
       children: [Array]
-    },
-    {
-      id: '6c4a2a99-3a9b-4cb8-9b81-916b914274dc',
-      key: '832',
-      name: [Object],
-      slug: [Object],
-      children: []
-    },
-    {
-      id: '72269543-b93b-45ad-b052-985e58123e25',
-      key: '1308',
-      name: [Object],
-      slug: [Object],
-      children: []
-    },
-    {
-      id: 'ed6c0df5-ab3e-47a0-8027-f341e9801d2d',
-      key: '304',
-      name: [Object],
-      slug: [Object],
-      children: []
-    },
-    {
-      id: '36b971d5-eead-4315-865f-5e3c9b8610cd',
-      key: '1154',
-      name: [Object],
-      slug: [Object],
-      children: []
     }
+    
   ]
 }
 global.fetch = jest.fn(()=>
@@ -48,12 +19,12 @@ Promise.resolve({
 })
 );
 
-it("category Service", async () => { 
+it("category Service with argument", async () => { 
   const res = await CategoryService.getCategoryList("en");
   expect(res.data.total).toEqual(data.total);
 })
 
-it("category Service", async () => { 
+it("category Service without argument", async () => { 
   const res = await CategoryService.getCategoryList( );
   expect(res.data.total).toEqual(data.total);
 })
@@ -63,6 +34,3 @@ it("handle exception in category Service", async () => {
   const result = await CategoryService.getCategoryList("en");
   expect(result).toEqual("API failure");
 })
-
-
-
