548bbb4699f06840464fb65820c2dd2fed7e3314
Samita Bajpai <samita.bajpai@hcl.com>
2021-10-09 01:31:52 +0530

    added apptus service

--- services/apptus/apptusService.js
+++ services/apptus/apptusService.js
@@ -6,19 +6,21 @@ class ApptusService {
 
   static getMegaMenu = async () => {
     const url = baseURL + categoryURL+pageName;
+   const params = new URLSearchParams({
+        window_first: windowFirst,
+        window_last: windowLast,
+        selected_category: selectedCategory,
+        sort_by: sortBy,
+        product_key: productKey,
+        category_tree: categoryTree,
+        'esales.market': esalesMarket,
+        'esales.customerKey': esalesCustomerKey,
+        'esales.sessionKey': esalesSessionKey
+       }).toString()
+       const parameters = JSON.stringify(params.replace(/%26/g, '&'))
+        
     try {
-      const result = await fetch(url + '?' + new URLSearchParams({
-       window_first: windowFirst,
-       window_last: windowLast,
-       selected_category: selectedCategory,
-       sort_by: sortBy,
-       product_key: productKey,
-       category_tree: categoryTree,
-       'esales.market': esalesMarket,
-       'esales.customerKey': esalesCustomerKey,
-       'esales.sessionKey': esalesSessionKey
-
-      }).toString());
+      const result = await fetch(url + '?' + JSON.stringify(parameters));
       const data = await result.json();
       return data;
     } catch (e) {
