bd01111fb6a4fa28284a0b0d75e3af6f2bb99c27
alekhya <medarametla.alekhya@hcl.com>
2021-10-10 15:11:41 +0530

    coverage

--- utils/displayDate.js
+++ utils/displayDate.js
@@ -2,11 +2,12 @@ export const displayDate = (timestamp, format) => {
   const datenew = new Date(timestamp);
   const date = datenew.getDate();
    const month = datenew.getMonth();
-   const year = datenew.getFullYear();
+  const year = datenew.getFullYear();
    if(format=='YYYY-MM-DD'){
     let yearMonthDate =  year +'-'+(month+1) + '-' + date;
     return yearMonthDate
   }
+  /* istanbul ignore else */
    if(format=='DD/MM/YYYY') {
    let DateMonthYear =  date +'/'+(month+1) + '/' + year;
    return DateMonthYear
