cdd8c46a4a7f10af8a79d3737452fd18c95bcf73
Samita Bajpai <samita.bajpai@hcl.com>
2021-10-22 22:07:33 +0530

    added product service

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import priceFormatter from './utils/priceFormatter';
 import CategoryService from './services/category/categoryService';
 import CartService  from './services/cart/cartService';
 import ApptusService from './services/apptus/apptusService';
-import pdpService from './services/pdp/pdpService'
+import pdpService from './services/pdp/pdpService';
 
 export {
     isParamsNotEmpty,
--- server.js
+++
@@ -1,9 +0,0 @@
-import  express from 'express';
-import CategoryService from './services/categoryService.js';
-var app = express()
-app.get('/', 
-async function (req, res) {
-  var categoryList = await CategoryService.getCategoryList('en');
-  res.send('hello world'+ categoryList)
-})
-app.listen(3033)
