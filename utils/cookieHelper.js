3044a351696f223c3955e2ca54c090849aa7e81b
Dmitry Humeniuk <dmitry.humeniuk@hcl.com>
2021-12-09 14:06:46 +0100

    handle apptus cookies

--- utils/cookieHelper.js
+++ utils/cookieHelper.js
@@ -1,28 +1,28 @@
 import {v4 as uuid} from 'uuid'
 import * as cookie from "cookie";
 
-export async function cookieHelper({ req, res }) {
-    const { cookies } = req
+export function cookieHelper({ req, res }) {
+    const { cookies } = req;
     let { esalesCustomerKey, esalesSessionKey } = cookies;
-    if (!esalesCustomerKey ) {
-        esalesCustomerKey = uuid()
-        esalesSessionKey = uuid()
+    if (!esalesCustomerKey || !esalesSessionKey) {
+        esalesCustomerKey = uuid();
+        esalesSessionKey = uuid();
         res.setHeader(
             "Set-Cookie",
-            cookie.serialize("esalesCustomerKey", esalesCustomerKey, {
+            [cookie.serialize("esalesCustomerKey", esalesCustomerKey, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV !== "development",
                 maxAge: 60 * 60,
                 sameSite: "strict",
                 path: "/",
             }),
-            cookie.serialize("esalesSessionKey", esalesCustomerKey, {
+            cookie.serialize("esalesSessionKey", esalesSessionKey, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV !== "development",
                 maxAge: 60 * 60,
                 sameSite: "strict",
                 path: "/",
-            })
+            })]
         );
     }
     return { esalesCustomerKey, esalesSessionKey }
